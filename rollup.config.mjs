8c692eceff741e8e3c463ac6ef2339c7b658e1df
jchu10 <junyichu10@gmail.com>
2025-11-11 01:01:14 -0800

    push to npm and try to find example bug

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,6 +3,14 @@ import postcss from 'rollup-plugin-postcss';
 
 const configs = makeRollupConfig("jsPsychPluginMathGames");
 
+// Modify the browser config to use umd format
+const browserConfig = configs.find(config => config.output.file && config.output.file.includes('index.browser.js'));
+if (browserConfig) {
+    browserConfig.output.format = 'umd';
+    browserConfig.output.name = 'jsPsychPluginMathGames';
+    browserConfig.output.globals = { jspsych: 'jsPsych' };
+}
+
 // Add postcss plugin to all configurations
 configs.forEach(config => {
     config.plugins.push(postcss({
